3b4e7df8a2395fd58636d469e4c56526fef78d59
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2024-01-05 03:43:24 +0530

    Added a Start Message

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-// script.js
+const startMsg = document.getElementById('startMsg');
+const response = document.getElementById('response');
+
+// when response element has text, hide startMsg
+response.addEventListener('DOMSubtreeModified', () => {
+	if (response.innerText) {
+		startMsg.style.display = 'none';
+	}
+});
