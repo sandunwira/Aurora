800e86614ecb4af36a2f49069f9a303935ccc768
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2024-01-08 04:20:27 +0530

    Updated Functions

--- src/status.js
+++ src/status.js
@@ -3,7 +3,7 @@ const statusImg = document.getElementById('statusImg');
 const serverStatus = document.getElementById('serverStatus');
 
 
-async function checkStatus() {
+async function botStatus() {
 	try {
 		const response = await fetch('http://192.168.1.142:5005');
 		if (response.ok) {
@@ -15,29 +15,29 @@ async function checkStatus() {
 	}
 }
 
-function changeStatus() {
-	checkStatus()
+function updateStatus() {
+	botStatus()
 		.then(status => {
 			if (status === 'Online') {
 				statusImg.style.filter = 'invert(44%) sepia(87%) saturate(545%) hue-rotate(62deg) brightness(93%) contrast(87%)';
 				serverStatus.title = 'Aurora is Online';
+				serverStatus.innerHTML = 'Online';
 			} else {
 				statusImg.style.filter = 'invert(22%) sepia(1%) saturate(0%) hue-rotate(290deg) brightness(97%) contrast(84%)';
 				serverStatus.title = 'Aurora is Offline';
+				serverStatus.innerHTML = 'Offline';
 			}
 		});
 }
 
 
 setInterval(() => {
-	changeStatus();
-	checkStatus()
-		.then(status => serverStatus.innerHTML = status);
+	updateStatus();
+	botStatus();
 }, 2000);
 
 
 window.onload = () => {
-	changeStatus();
-	checkStatus()
-		.then(status => serverStatus.innerHTML = status);
+	updateStatus();
+	botStatus();
 };
