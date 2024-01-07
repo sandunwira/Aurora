6f87903beb73ec1c5aa9fdc8678d8a21cc9e7560
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2024-01-07 11:47:16 +0530

    Fixed Online/Offline

--- src/status.js
+++ src/status.js
@@ -1,27 +1,52 @@
 const statusImg = document.getElementById('statusImg');
 const serverStatus = document.getElementById('serverStatus');
 
-function checkStatus() {
-	return fetch('http://192.168.1.142:5005')
-		.then(response => {
-			if (response.ok) {
+async function checkStatus() {
+	try {
+		const response = await fetch('http://192.168.1.142:5005');
+		if (response.ok) {
+			return 'Online';
+		}
+	} catch (error) {
+		return 'Offline';
+	}
+}
+
+// change statusImg filter based on the return value of checkStatus()
+function changeStatusImg() {
+	checkStatus()
+		.then(status => {
+			if (status === 'Online') {
 				statusImg.style.filter = 'invert(44%) sepia(87%) saturate(545%) hue-rotate(62deg) brightness(93%) contrast(87%)';
-				return 'Online';
 			} else {
-				statusImg.style.filter = 'invert(56%) sepia(1%) saturate(1914%) hue-rotate(317deg) brightness(89%) contrast(96%)';
-				return 'Offline';
+				statusImg.style.filter = 'invert(22%) sepia(1%) saturate(0%) hue-rotate(290deg) brightness(97%) contrast(84%)';
 			}
 		});
 }
 
+
 setInterval(() => {
 	checkStatus()
-		.then(status => serverStatus.innerHTML = status)
-		.catch(error => console.error(error));
-}, 3000);
+		.then(status => serverStatus.innerHTML = status);
+}, 2000);
+
+setInterval(() => {
+	changeStatusImg();
+}, 2000);
+
+setInterval (() => {
+	console.clear();
+}, 2100);
 
 window.onload = () => {
 	checkStatus()
-		.then(status => serverStatus.innerHTML = status)
-		.catch(error => console.error(error));
+		.then(status => serverStatus.innerHTML = status);
+};
+
+window.onload = () => {
+	changeStatusImg();
+};
+
+window.onload = () => {
+	console.clear();
 };
