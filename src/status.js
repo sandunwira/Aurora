59795bd3609892a54a11f13c2154a308f8d23e47
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2024-01-09 22:34:03 +0530

    Enable Notifications | Added a Network Indicator

--- src/status.js
+++ src/status.js
@@ -1,6 +1,7 @@
 const statusIndicator = document.getElementById('statusIndicator');
 const statusImg = document.getElementById('statusImg');
 const serverStatus = document.getElementById('serverStatus');
+const networkIndicator = document.getElementById('networkIndicator');
 
 
 async function botStatus() {
@@ -31,13 +32,45 @@ function updateStatus() {
 }
 
 
+async function networkStatus() {
+	return fetch('https://github.com/sandunwira/Aurora', {
+		method: 'HEAD',
+		mode: 'no-cors'
+	})
+		.then(() => {
+			return 'Online';
+		})
+		.catch(() => {
+			return 'Offline';
+		});
+}
+
+function updateNetworkStatus() {
+	networkStatus()
+		.then(status => {
+			if (status === 'Online') {
+				networkIndicator.style.filter = 'invert(44%) sepia(87%) saturate(545%) hue-rotate(62deg) brightness(93%) contrast(87%)';
+				networkIndicator.title = 'You are Online';
+			} else {
+				networkIndicator.style.filter = 'invert(22%) sepia(1%) saturate(0%) hue-rotate(290deg) brightness(97%) contrast(84%)';
+				networkIndicator.title = 'You are Offline';
+			}
+		});
+}
+
+
+
 setInterval(() => {
 	updateStatus();
 	botStatus();
+	networkStatus();
+	updateNetworkStatus();
 }, 2000);
 
 
 window.onload = () => {
 	updateStatus();
 	botStatus();
+	networkStatus();
+	updateNetworkStatus();
 };
